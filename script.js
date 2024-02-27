44a1a96d256f64799a0df17ce1eb76e1fc7ec222
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-02-27 19:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius=document.getElementById('radius').value;
+	//4/3 π r³
+	let volume=(4/3)*3.14*(radius**3);
+	document.getElementById('volume').value=volume;
+ 
 } 
+
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
